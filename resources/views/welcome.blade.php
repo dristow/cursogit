ac994bdd9ff90e8539d5af73095a68228dd96de1
dristow <douglasristow@gmail.com>
2023-05-17 14:16:44 -0300

    First Commit Course Git

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cursos da Git</title>
+</head>
+<body>
+    <ul>
+        <li>Versionamento</li>
+        <li>Comandos</li>
+        <li>Deploy</li>
+    </ul>
+</body>
+</html>
