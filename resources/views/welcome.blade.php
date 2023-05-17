ccef0948c47fe78b4a97a4b1728fe3cbebf19d9a
dristow <douglasristow@gmail.com>
2023-05-17 17:02:15 -0300

    CORRECAO index welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,15 +2,17 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Cursos da Git nova Branch Título</title>
+    <title>Curso Git Branch Título</title>
 </head>
 <body>
+    <h1>Merge da branch master com a branch titulo</h1>
     <ul>
         <li>Versionamento</li>
         <li>Comandos</li>
         <li>Deploy</li>
         <li class="column">Integração</li>
         <li>Status</li>
+        <li>SETUP</li>
     </ul>
 </body>
 </html>
